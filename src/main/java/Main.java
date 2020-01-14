4fe77b5b49c5bc87bdf1f710a75a8fde54a4de20
Terry <terry.zhou@student.hu.nl>
2020-01-14 13:41:40 +0100

    Generate triggernames

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
+import generate.RuleGenerator;
+
 public class Main {
+    public static void main(String[] args) {
+        new RuleGenerator();
+    }
 }
--- src/main/java/generate/RuleGenerator.java
+++ src/main/java/generate/RuleGenerator.java
@@ -4,9 +4,25 @@ public class RuleGenerator {
     private String generatedCode;
     private String triggerName;
 
+
+    // TODO: change static values
+    private String applicationName = "BRG";
+    private String projectName = "VBMG";
+    private String attribute = "product";
+    private String ruleTypeCode = "arng";
+    private String tableName = "products";
+    private String tableAttribute = "productCode";
+
+
+    public RuleGenerator() {
+        generateTriggerName();
+        System.out.println(this.triggerName);
+    }
+
+
     private void generateTriggerName() {
         System.out.println("TriggerName is being generated...");
-
+        this.triggerName = (String.format("%s_%s_%s_trigger_%s", this.applicationName, this.projectName, this.attribute.substring(0, 4), this.ruleTypeCode)).toUpperCase();
     }
 
     private void generateCode() {
