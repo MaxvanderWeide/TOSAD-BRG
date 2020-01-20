848a2d23a647c6fdc3019f98be27e8b1afbe3bd7
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-01-20 17:09:06 +0100

    Added Compare and List test data

--- src/main/resources/public/scripts/comparator-controller.js
+++ src/main/resources/public/scripts/comparator-controller.js
@@ -13,5 +13,35 @@ var Types = {
             "new Array(" +
             "document.getElementById(\"custInput1\").value" + "," +
             "document.getElementById(\"custInput2\").value)"
+    },
+    Compare: {
+        block:
+            "var custInput1 = document.createElement(\"INPUT\");" +
+            "custInput1.setAttribute(\"type\", \"text\");" +
+            "custInput1.setAttribute(\"id\", \"custInput1\");" +
+            "document.getElementById(\"comparatorStep\").appendChild(custInput1);" +
+            "var custInput2 = document.createElement(\"INPUT\");" +
+            "custInput2.setAttribute(\"type\", \"text\");" +
+            "custInput2.setAttribute(\"id\", \"custInput2\");" +
+            "document.getElementById(\"comparatorStep\").appendChild(custInput2);",
+        reval:
+            "new Array(" +
+            "document.getElementById(\"custInput1\").value" + "," +
+            "document.getElementById(\"custInput2\").value)"
+    },
+    List: {
+        block:
+            "var custInput1 = document.createElement(\"INPUT\");" +
+            "custInput1.setAttribute(\"type\", \"text\");" +
+            "custInput1.setAttribute(\"id\", \"custInput1\");" +
+            "document.getElementById(\"comparatorStep\").appendChild(custInput1);" +
+            "var custInput2 = document.createElement(\"INPUT\");" +
+            "custInput2.setAttribute(\"type\", \"text\");" +
+            "custInput2.setAttribute(\"id\", \"custInput2\");" +
+            "document.getElementById(\"comparatorStep\").appendChild(custInput2);",
+        reval:
+            "new Array(" +
+            "document.getElementById(\"custInput1\").value" + "," +
+            "document.getElementById(\"custInput2\").value)"
     }
 };
