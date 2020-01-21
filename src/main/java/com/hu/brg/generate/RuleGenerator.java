a8118afa0d24c78f794de3cbd202f78c8cf6a0f6
Bart0110 <Bart0110@users.noreply.github.com>
2020-01-21 18:14:59 +0100

    Fix incorrect iterator comparison

--- src/main/java/com/hu/brg/generate/RuleGenerator.java
+++ src/main/java/com/hu/brg/generate/RuleGenerator.java
@@ -38,7 +38,7 @@ public class RuleGenerator {
         for (int i = 0; i < triggerEvents.size(); i++) {
             String event = triggerEvents.get(i);
             stringBuilder.append(event);
-            if (i != triggerEvents.size()) {
+            if (i != triggerEvents.size() - 1) {
                 stringBuilder.append(" OR ");
             }
         }
