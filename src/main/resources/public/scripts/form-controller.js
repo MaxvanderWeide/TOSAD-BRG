a794b6be62cc0c62afad336321ac06ef4684f175
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-01-21 22:13:02 +0100

    Changed var to let or const

--- src/main/resources/public/scripts/form-controller.js
+++ src/main/resources/public/scripts/form-controller.js
@@ -1,6 +1,6 @@
 function loadFromStorage() {
     if (sessionStorage.getItem("values") != null) {
-        var values = JSON.parse(sessionStorage.getItem("values"));
+        const values = JSON.parse(sessionStorage.getItem("values"));
         document.getElementById("dbEngine").options[dbEngine.selectedIndex].value = values['engine'];
         document.getElementById("dbInputHost").value = values['host'];
         document.getElementById("dbName").value = values['dbName'];
@@ -12,16 +12,16 @@ function loadFromStorage() {
 }
 
 function createConnection() {
-    var dbEngine = document.getElementById("dbEngine");
-    var dbEngineName = dbEngine.options[dbEngine.selectedIndex].value;
-    var dbHost = document.getElementById("dbInputHost").value;
-    var dbName = document.getElementById("dbName").value;
-    var dbPort = document.getElementById("dbInputPort").value;
-    var dbService = document.getElementById("dbInputService").value;
-    var dbUser = document.getElementById("dbInputUser").value;
-    var dbPassword = document.getElementById("dbInputPassword").value;
+    const dbEngine = document.getElementById("dbEngine");
+    const dbEngineName = dbEngine.options[dbEngine.selectedIndex].value;
+    const dbHost = document.getElementById("dbInputHost").value;
+    const dbName = document.getElementById("dbName").value;
+    const dbPort = document.getElementById("dbInputPort").value;
+    const dbService = document.getElementById("dbInputService").value;
+    const dbUser = document.getElementById("dbInputUser").value;
+    const dbPassword = document.getElementById("dbInputPassword").value;
 
-    var values = {};
+    let values = {};
     values["engine"] = dbEngineName;
     values["dbName"] = dbName;
     values["host"] = dbHost;
@@ -31,7 +31,7 @@ function createConnection() {
     values["password"] = dbPassword;
     values = JSON.stringify(values);
 
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
             if (this.status == 200) {
@@ -49,15 +49,15 @@ function createConnection() {
 }
 
 function fillTargetTables() {
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log("Connected!");
             console.log('GET SUCCESS: ' + this.responseText);
-            var responseJSON = JSON.parse(this.responseText);
-            var selection = document.getElementById("tableSelection");
-            for (var k in responseJSON.Tables) {
-                var option = document.createElement("option");
+            const responseJSON = JSON.parse(this.responseText);
+            const selection = document.getElementById("tableSelection");
+            for (const k in responseJSON.Tables) {
+                const option = document.createElement("option");
                 option.text = responseJSON.Tables[k];
                 selection.add(option)
             }
@@ -70,14 +70,14 @@ function fillTargetTables() {
 
 
 function fillTypes() {
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log('GET SUCCESS: ' + this.responseText);
-            var responseJSON = JSON.parse(this.responseText);
-            var selection = document.getElementById("typeSelection");
-            for (var k in responseJSON.Types) {
-                var option = document.createElement("option");
+            const responseJSON = JSON.parse(this.responseText);
+            const selection = document.getElementById("typeSelection");
+            for (const k in responseJSON.Types) {
+                const option = document.createElement("option");
                 option.text = k;
                 selection.add(option)
             }
@@ -88,15 +88,15 @@ function fillTypes() {
 }
 
 function fillTargetAttributes(tableSelection) {
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log('GET SUCCESS: ' + this.responseText);
-            var responseJSON = JSON.parse(this.responseText);
-            var selection = document.getElementById("attributeSelection");
+            const responseJSON = JSON.parse(this.responseText);
+            const selection = document.getElementById("attributeSelection");
             selection.options.length = 1;
-            for (var k in responseJSON.Attributes) {
-                var option = document.createElement("option");
+            for (const k in responseJSON.Attributes) {
+                const option = document.createElement("option");
                 option.text = k + ' - ' + responseJSON.Attributes[k];
                 selection.add(option)
             }
@@ -108,15 +108,15 @@ function fillTargetAttributes(tableSelection) {
 }
 
 function fillOperators(type) {
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log('GET SUCCESS: ' + this.responseText);
-            var responseJSON = JSON.parse(this.responseText);
-            var selection = document.getElementById("operatorSelection");
+            const responseJSON = JSON.parse(this.responseText);
+            const selection = document.getElementById("operatorSelection");
             selection.options.length = 1;
-            for (var k in responseJSON.Operators) {
-                var option = document.createElement("option");
+            for (const k in responseJSON.Operators) {
+                const option = document.createElement("option");
                 option.text = responseJSON.Operators[k];
                 selection.add(option)
             }
@@ -128,15 +128,15 @@ function fillOperators(type) {
 
 function fillComparators(operator, type) {
 
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log('GET SUCCESS: ' + this.responseText);
-            var responseJSON = JSON.parse(this.responseText);
-            var selection = document.getElementById("comparatorSelection");
+            const responseJSON = JSON.parse(this.responseText);
+            const selection = document.getElementById("comparatorSelection");
             selection.options.length = 1;
-            for (var k in responseJSON) {
-                var option = document.createElement("option");
+            for (const k in responseJSON) {
+                const option = document.createElement("option");
                 option.setAttribute("reval", responseJSON[k].CodeReval);
                 option.setAttribute("block", responseJSON[k].CodeBlock);
                 option.text = k;
@@ -149,30 +149,30 @@ function fillComparators(operator, type) {
 }
 
 function saveRule() {
-    var selectedTable = document.getElementById("tableSelection");
-    var selectedTableName = selectedTable.options[selectedTable.selectedIndex].value;
+    const selectedTable = document.getElementById("tableSelection");
+    const selectedTableName = selectedTable.options[selectedTable.selectedIndex].value;
 
-    var selectedType = document.getElementById("typeSelection");
-    var selectedTypeName = selectedType.options[selectedType.selectedIndex].value;
+    const selectedType = document.getElementById("typeSelection");
+    const selectedTypeName = selectedType.options[selectedType.selectedIndex].value;
 
-    var selectedTargetAttribute = document.getElementById("attributeSelection");
-    var selectedTargetAttributeName = selectedTargetAttribute.options[selectedTargetAttribute.selectedIndex].value;
+    const selectedTargetAttribute = document.getElementById("attributeSelection");
+    const selectedTargetAttributeName = selectedTargetAttribute.options[selectedTargetAttribute.selectedIndex].value;
 
-    var selectedOperator = document.getElementById("operatorSelection");
-    var selectedOperatorName = selectedOperator.options[selectedOperator.selectedIndex].value;
+    const selectedOperator = document.getElementById("operatorSelection");
+    const selectedOperatorName = selectedOperator.options[selectedOperator.selectedIndex].value;
 
-    var selectedComparator = document.getElementById("comparatorSelection");
-    var selectedComparatorName = selectedComparator.options[selectedComparator.selectedIndex].value;
+    const selectedComparator = document.getElementById("comparatorSelection");
+    const selectedComparatorName = selectedComparator.options[selectedComparator.selectedIndex].value;
 
-    var comparator = document.getElementById("comparatorSelection");
+    const comparator = document.getElementById("comparatorSelection");
 
-    var ruleValues = [];
+    const ruleValues = [];
 
     for(const li of document.querySelectorAll("ul.attributes-list li")) {
         ruleValues.push(li.textContent);
     }
 
-    var values = {};
+    let values = {};
     values["ruleName"] = document.getElementById("ruleName").value;
     values["tableName"] = selectedTableName;
     values["typeName"] = selectedTypeName;
@@ -183,7 +183,7 @@ function saveRule() {
     values["values"] = ruleValues;
     values = JSON.stringify(values);
 
-    var xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
             if (this.status == 201) {
