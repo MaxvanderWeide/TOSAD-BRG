6a9c30f27000b9feb518ba4ab7eb161b90a8f660
Terry <terry.zhou@student.hu.nl>
2020-01-22 16:38:07 +0100

    Generate: show all ruletypes dynamically

--- src/main/resources/public/generate/scripts/generator-controller.js
+++ src/main/resources/public/generate/scripts/generator-controller.js
@@ -1,27 +1,56 @@
 loadRules();
 
 function loadRules() {
-    const values = {};
-    values["typeCode"] = "ACMP";
-    values["ruleType"] = "Compare";
-    values["subType"] = "Attribute";
-    values["table"] = "Producten";
-    values["attribute"] = "prijs";
-    values["operator"] = "LessThan";
-    values["errorMessage"] = "Error!!!!!";
-    values["errorCode"] = "-20000";
-    values["status"] = "Generated";
-    values["values"] = ["100", "100"];
-
-    // console.log(values);
-
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
             $(".rule-items-wrapper").show();
             $(".spinner-holder").hide();
             if(this.status == 200) {
-                console.log('rules: ' + this.responseText);
+                const responseJSON = JSON.parse(this.responseText);
+                const values = ["hoi", "doei"]; // TODO - change static values
+                for (const k in responseJSON) {
+                    const item = responseJSON[k];
+                    const itemWrapper = document.createElement("div");
+                    itemWrapper.setAttribute("class", "rule-item");
+                    const ruleWrapper = document.createElement("div");
+                    ruleWrapper.setAttribute("class", "rule-wrapper");
+                    const ruleInfoWrapper = document.createElement("div");
+                    ruleInfoWrapper.setAttribute("class", "rule-info-wrapper");
+                    const ruleLeftBlock = document.createElement("div");
+                    ruleLeftBlock.setAttribute("class", "rule-info-left-block");
+                    const ruleRightBlock = document.createElement("div");
+                    ruleRightBlock.setAttribute("class", "rule-info-right-block");
+                    const ruleBottomBlock = document.createElement("div");
+                    ruleBottomBlock.setAttribute("class", "rule-info-bottom-block");
+                    const generatedRuleWrapper = document.createElement("div");
+                    generatedRuleWrapper.setAttribute("class", "generated-rule-wrapper");
+                    const ul = document.createElement("ul");
+
+                    for(const value of values) {
+                        $(ul).append("<li>"+value+"</li>")
+                    }
+
+                    $(ruleLeftBlock).append("<label>Type code</label><input type='text' value='"+ item["Operator"] +"'>");
+                    $(ruleLeftBlock).append("<label>Type</label><input type='text' value='"+ item["Type"] +"'>");
+                    $(ruleLeftBlock).append("<label>Error code</label><input type='text' value='"+ item["ErrorCode"] +"'>");
+                    $(ruleLeftBlock).append("<label>Error message</label><input type='text' value='"+ item["ErrorMessage"] +"'>");
+                    $(ruleRightBlock).append("<label>Table</label><input type='text' value='"+ item["Table"] +"'>");
+                    $(ruleRightBlock).append("<label>Attribute</label><input type='text' value='"+ item["Attribute"] +"'>");
+                    $(ruleRightBlock).append("<label>Operator</label><input type='text' value='"+ item["Operator"] +"'>");
+                    $(ruleBottomBlock).append("<label>Values</label>");
+                    $(ruleBottomBlock).append(ul);
+                    $(ruleInfoWrapper).append(ruleLeftBlock, ruleRightBlock, ruleBottomBlock);
+                    $(generatedRuleWrapper).append("<span class='generated-rule-title'>Generated rule</span>");
+                    $(generatedRuleWrapper).append("<div class='generated-rule'>create trigger blablabla</div>");
+                    $(itemWrapper).append("<div class='rule-name'>"+k+"</div>");
+                    $(ruleWrapper).append(ruleInfoWrapper, generatedRuleWrapper);
+                    $(itemWrapper).append(ruleWrapper);
+                    $(itemWrapper).append("<div class='rule-generator-wrapper'><button onclick='generateCode(this)'>Generate code</button></div>");
+
+
+                    $("div.rule-items-wrapper").append(itemWrapper);
+                }
             }
         }
     };
@@ -32,6 +61,7 @@ function loadRules() {
 
 function generateCode(element) {
     const ruleName = element.closest("div.rule-item").querySelector("div.rule-name").textContent;
+    element.closest("div.rule-item").querySelector("div.generated-rule").textContent = "create trigger lalalala";
 
     console.log(ruleName);
 }
