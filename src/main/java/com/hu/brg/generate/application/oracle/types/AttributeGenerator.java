MaxvanderWeide/TOSAD-BRG

commit 7ab455294e87d46cbfd19977ebd019ea2be0fc42

Fixed minor linting issues

src/main/java/com/hu/brg/generate/application/oracle/types/AttributeGenerator.java
@@ -13,7 +13,7 @@ public class AttributeGenerator implements TypeGenerator {
 
     @Override
     public void fillStringTemplate(ST stringTemplate, List<Attribute> attributeList, String groupPath) {
-        if (attributeList.size() == 0) {
+        if (attributeList.isEmpty()) {
             throw new IllegalStateException("AttributeList cannnot be empty");
         }
 
@@ -28,7 +28,7 @@ public class AttributeGenerator implements TypeGenerator {
 
             stringTemplate.add("value_min", attribute.getAttributeValues().get(0).sanitizedValue());
             stringTemplate.add("value_max", attribute.getAttributeValues().get(1).sanitizedValue());
-        } else if (attribute.getRule().getRuleType().isList() && attribute.getAttributeValues().size() > 0) {
+        } else if (attribute.getRule().getRuleType().isList() && attribute.getAttributeValues().isEmpty()) {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append("(");
 

src/main/java/com/hu/brg/define/persistence/BaseDAO.java
@@ -26,15 +26,17 @@ public abstract class BaseDAO {
         try {
             if (connection == null || connection.isClosed() || !connection.isValid(10)) {
                 switch (dbEngine) {
-                    case ORACLE:
-                    default: {
+                    case ORACLE: {
                         OracleDataSource ods = new OracleDataSource();
                         ods.setURL(String.format("jdbc:oracle:thin:@//%s:%d/%s", host, port, serviceName)); // jdbc:oracle:thin@//[hostname]:[port]/[DB service name]
                         ods.setUser(user); // [username]
                         ods.setPassword(password); // [password]
                         connection = ods.getConnection();
                         connection.setAutoCommit(true);
+                        break;
                     }
+                    default:
+                        return null;
                 }
             }
         } catch (SQLException e) {

src/main/java/com/hu/brg/generate/persistence/BaseDAO.java
@@ -25,14 +25,17 @@ public abstract class BaseDAO {
     protected Connection getConnection(DBEngine dbEngine, String host, int port, String serviceName, String user, String password) {
         try {
             switch (dbEngine) {
-                case ORACLE:
-                default: {
+                case ORACLE: {
                     OracleDataSource ods = new OracleDataSource();
                     ods.setURL(String.format("jdbc:oracle:thin:@//%s:%d/%s", host, port, serviceName)); // jdbc:oracle:thin@//[hostname]:[port]/[DB service name]
                     ods.setUser(user); // [username]
                     ods.setPassword(password); // [password]
                     connection = ods.getConnection();
+                    connection.setAutoCommit(true);
+                    break;
                 }
+                default:
+                    return null;
             }
         } catch (SQLException e) {
             return null;

src/main/java/com/hu/brg/service/persistance/BaseDAO.java
@@ -25,15 +25,17 @@ public abstract class BaseDAO {
         try {
             if (connection == null || connection.isClosed() || !connection.isValid(10)) {
                 switch (dbEngine) {
-                    case ORACLE:
-                    default: {
+                    case ORACLE: {
                         OracleDataSource ods = new OracleDataSource();
                         ods.setURL(String.format("jdbc:oracle:thin:@//%s:%d/%s", host, port, serviceName)); // jdbc:oracle:thin@//[hostname]:[port]/[DB service name]
                         ods.setUser(user); // [username]
                         ods.setPassword(password); // [password]
                         connection = ods.getConnection();
                         connection.setAutoCommit(true);
+                        break;
                     }
+                    default:
+                        return null;
                 }
             }
         } catch (SQLException e) {

commit b2008756de996a28f6ae28898692c9c023b5da60

Fix issue introduced by commit 7ab4552 Looking at you Max 👀

src/main/java/com/hu/brg/generate/application/oracle/types/AttributeGenerator.java
@@ -28,7 +28,7 @@ public class AttributeGenerator implements TypeGenerator {
 
             stringTemplate.add("value_min", attribute.getAttributeValues().get(0).sanitizedValue());
             stringTemplate.add("value_max", attribute.getAttributeValues().get(1).sanitizedValue());
-        } else if (attribute.getRule().getRuleType().isList() && attribute.getAttributeValues().isEmpty()) {
+        } else if (attribute.getRule().getRuleType().isList() && !attribute.getAttributeValues().isEmpty()) {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append("(");
 